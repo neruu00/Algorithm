f18d12be2b948036a2425d496c0dcd9dd298075b
neruu00 <dnwogus4260@naver.com>
2026-02-06 14:43:57 +0900

    26.02.06 오전

--- SW_Expert_Academy/swea_4130.java
+++ SW_Expert_Academy/swea_4130.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class swea_4130 {
 	static int[] gears = new int[4];
 
 	public static void main(String[] args) throws Exception {
