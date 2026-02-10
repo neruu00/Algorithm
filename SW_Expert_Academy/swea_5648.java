f3df3bb384da80ee0089c22a1e14966863af27b2
neruu00 <dnwogus4260@naver.com>
2026-02-11 08:43:03 +0900

    26.02.11

--- SW_Expert_Academy/swea_5648.java
+++ SW_Expert_Academy/swea_5648.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+
 public class swea_5648 {
-    static int N, ans;
-    static List<Atom> atoms;
-    static int dx[] = {0, 0, -1, 1};
-    static int dy[] = {1, -1, 0, 0};  
-    static int[][] map = new int[4001][4001];
-
-    static class Atom {
-        int x, y, d, k;
-        Atom(int x, int y, int d, int k) {
-            this.x = x; this.y = y; this.d = d; this.k = k;
-        }
-    }
-
-    static void solve() {
-        for (int t = 0; t <= 4000; t++) {
-            if (atoms.size() < 2) break;
-            
+	static int N, minDist, ans;
+	static List<Atom> atoms;
+	static int dx[] = {0, 0, -1, 1};
+	static int dy[] = {1, -1, 0, 0};
+	
+	static class Atom {
+		int x, y, d, k;
+		Atom(int x, int y, int d, int k) {
+			this.x = x;
+			this.y = y;
+			this.d = d;
+			this.k = k;
+		}
+	}
+	
+	static void solve() {
+		while(atoms.size() > 1) {
+			
 			int min = Integer.MAX_VALUE;
 			for(int i = 0; i < atoms.size()-1; i++) {
 				Atom a = atoms.get(i);
@@ -34,51 +36,69 @@ public class swea_5648 {
 			
 			if(min == Integer.MAX_VALUE) break;
 			min /= 2;
+			
+			for(int i = atoms.size()-1; i >= 0; i--) {
+				Atom a = atoms.get(i);
+				a.x += dx[a.d] * min;
+				a.y += dy[a.d] * min;
+				if(a.x < -2000 || a.x > 2000 || a.y < -2000 || a.y > 2000) {
+					atoms.remove(i);
+				}
+			}
+			
+			boolean flag = false;
+			int remove[] = new int[atoms.size()];
+			
+			for(int i = 0; i < atoms.size()-1; i++) {
+				Atom A = atoms.get(i);
+				for(int j = i+1; j < atoms.size(); j++) {
+					Atom B = atoms.get(j);
+					if(A.x==B.x && A.y==B.y) {
+						remove[i] = 1;
+						remove[j] = 1;
+						flag = true;
+					}
+				}
+			}
+			
+			if(flag) {
+				for(int i = atoms.size()-1; i >= 0; i--) {
+					if(remove[i] == 1) {
+						ans+=atoms.get(i).k;
+						atoms.remove(i);
+					} else if(remove[i] == 2) {
+						atoms.remove(i);
+					}
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
 
-            for (int i = atoms.size() - 1; i >= 0; i--) {
-            	Atom a = atoms.get(i);
-                a.x += dx[a.d] * min;
-                a.y += dy[a.d];
-
-                if (a.x < 0 && a.x > 4000 && a.y < 0 && a.y > 4000) {
-                    atoms.remove(i);
-                    continue;
-                } else {
-                    map[a.x][a.y] += a.k;
-                }
-            }
-
-            
-            for (int i = atoms.size() - 1; i >= 0; i--) {
-                Atom a = atoms.get(i);
-                map[a.y][a.x]++;
-            }
-            
-            for (Atom a : atoms) {
-                map[a.x][a.y] = 0;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine().trim());
-            atoms = new ArrayList<>();
-            for (int i = 0; i < N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int x = (Integer.parseInt(st.nextToken()) + 1000) * 2;
-                int y = (Integer.parseInt(st.nextToken()) + 1000) * 2;
-                int d = Integer.parseInt(st.nextToken());
-                int k = Integer.parseInt(st.nextToken());
-                atoms.add(new Atom(x, y, d, k));
-            }
-
-            ans = 0;
-            solve();
-            System.out.println("#" + tc + " " + ans);
-        }
-    }
+		for(int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
+			atoms = new ArrayList<>();
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken())*2;
+				int y = Integer.parseInt(st.nextToken())*2;
+				int d = Integer.parseInt(st.nextToken());
+				int k = Integer.parseInt(st.nextToken());
+				atoms.add(new Atom(x, y, d, k));
+			}
+			
+			ans = 0;
+			solve();
+			
+			sb.append('#').append(tc).append(' ').append(ans).append('\n');
+		}	
+		System.out.println(sb);
+	}
 }
